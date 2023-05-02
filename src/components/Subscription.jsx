73270ecd770e08069613628b610989e815fff895
Enver Can Oude Hergelink <enver.hergelink@gmail.com>
2023-05-02 11:41:58 +0300

    added a visual div for the subscription box

--- src/components/Subscription.jsx
+++ src/components/Subscription.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Navigate } from 'react-router-dom';
-import style from '../styles/Subscription.module.css'
+import style from '../styles/Subscription.module.css';
 
 export default function Subscription() {
   const [redirect, setRedirect] = useState(false);
@@ -11,7 +11,6 @@ export default function Subscription() {
   // Paddle.Setup({
   //   vendor: process.env.VENDOR_ID,
   // });
-  
 
   async function handlePayment(e) {
     e.preventDefault();
@@ -40,37 +39,39 @@ export default function Subscription() {
   return (
     <div className={style.Container} id={style.subscribe}>
       <h3 id={style.callToAction}>
-          Don't just read,
-          <br /> <span>create your own!</span>
-        </h3>
-      
-      <h1 id={style.title}>14-Day Free Trial</h1>
+        Don't just read,
+        <br /> <span>create your own!</span>
+      </h3>
+      <div>
+        <h2 id={style.title}>14-Day Free Trial</h2>
 
-      <form id={style.subscribeForm} onSubmit={handlePayment}>
-        <h3 style={{ fontSize: '1.3rem', textAlign: 'center' }}>
-          Subscription
-        </h3>
-        <hr
-          style={{
-            width: '20%',
-            height: '4px',
-            backgroundColor: 'white',
-            margin: '0 auto',
-          }}
-        />
-        <h3 style={{ textAlign: 'center', fontSize: '2rem' }}>
-          $3.99 <span style={{ color: 'grey', fontSize: '1rem' }}>/month</span>
-        </h3>
-        <ul style={{ margin: '0 auto', listStyle: 'none' }}>
-          <li>✓ Unlimited Stories</li>
-          <li>✓ Download PDF's</li>
-          <li>✓ Publish to Stories</li>
-          <li>✓ Cancel Anytime</li>
-        </ul>
+        <form id={style.subscribeForm} onSubmit={handlePayment}>
+          <h3 style={{ fontSize: '1.3rem', textAlign: 'center' }}>
+            Subscription
+          </h3>
+          <hr
+            style={{
+              width: '20%',
+              height: '4px',
+              backgroundColor: 'white',
+              margin: '0 auto',
+            }}
+          />
+          <h3 style={{ textAlign: 'center', fontSize: '2rem' }}>
+            $3.99{' '}
+            <span style={{ color: 'grey', fontSize: '1rem' }}>/month</span>
+          </h3>
+          <ul style={{ margin: '0 auto', listStyle: 'none' }}>
+            <li>✓ Unlimited Stories</li>
+            <li>✓ Download PDF's</li>
+            <li>✓ Publish to Stories</li>
+            <li>✓ Cancel Anytime</li>
+          </ul>
 
-        <button id={style.subscribeButton}>Subscribe</button>
-        <p id={style.errorP}>{alert ? 'Connection Failed' : null}</p>
-      </form>
+          <button id={style.subscribeButton}>Subscribe</button>
+          <p id={style.errorP}>{alert ? 'Connection Failed' : null}</p>
+        </form>
+      </div>
     </div>
   );
 }
